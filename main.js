f952119386bda3e59a317a24ed3ac50ca86cc594
VicenzoMF <vicenzomf@gmail.com>
2025-06-12 00:48:02 -0300

    2o nivel

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const fs = require('fs');
-const { pageTableStrategies } = require('./pageTableHandler');
+const fs = require("fs");
+const { pageTableStrategies } = require("./pageTableHandler");
 
 console.log("---=== Iniciando Simulador de Memória ===---");
 
-const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+const config = JSON.parse(fs.readFileSync("config.json", "utf8"));
 console.log("Configurações carregadas:", config);
 
 const virtualAddressSpaceSize = Math.pow(2, config.virtualAddressBits);
@@ -17,11 +17,23 @@ console.log(`\nTamanho da Página: ${pageSize} bytes`);
 console.log(`Número de Páginas Virtuais: ${numVirtualPages}`);
 console.log(`Número de Molduras Físicas: ${numPhysicalFrames}`);
 
+const text_end = config.segments.text;
+const data_end = text_end + config.segments.data;
+const stack_end = data_end + config.segments.stack;
+
+const bss_size = virtualAddressSpaceSize - (config.segments.text + config.segments.data + config.segments.stack);
+console.log(`\nLayoutde memória definido:`);
+console.log(` -> .text: 0 - ${text_end - 1}`);
+console.log(` -> .data: ${text_end} - ${data_end - 1}`);
+console.log(` -> .stack: ${data_end} - ${stack_end - 1}`);
+console.log(` -> .bss: ${stack_end} - ${virtualAddressSpaceSize - 1} (Tamanho: ${bss_size})`);
+
 const p = config.pageSizeBits;
 
-const virtualAddresses = fs.readFileSync('entrada.txt', 'utf8')
-  .split('\n')
-  .filter(line => line.trim() !== '')
+const virtualAddresses = fs
+  .readFileSync("entrada.txt", "utf8")
+  .split("\n")
+  .filter((line) => line.trim() !== "")
   .map(Number);
 
 console.log("\nEndereços virtuais a serem processados:", virtualAddresses);
@@ -31,39 +43,46 @@ const strategy = pageTableStrategies[config.pageTableType];
 const physicalMemory = new Array(numPhysicalFrames).fill(-1);
 const pageTable = strategy.initialize(numVirtualPages, config);
 
-console.log("\nMemória física inicializada")
-fs.writeFileSync('saida.txt', '');
+console.log("\nMemória física inicializada");
+fs.writeFileSync("saida.txt", "");
 
 console.log("\n--- Início da SImulação ---");
 
-virtualAddresses.forEach(virtualAddress => {
+virtualAddresses.forEach((virtualAddress) => {
   try {
-    console.log(`\n\n Analisando endereço virtual: ${virtualAddress}`)
-    const frameNumber = strategy.translate(virtualAddress, pageTable, physicalMemory, config)
+    console.log(`\n\n Analisando endereço virtual: ${virtualAddress}`);
+
+    let segmentName;
+    if (virtualAddress < text_end) {
+      segmentName = ".text";
+    } else if (virtualAddress < data_end) {
+      segmentName = ".data";
+    } else if (virtualAddress < stack_end) {
+      segmentName = ".stack";
+    } else {
+      segmentName = ".bss";
+    }
+    console.log(` -> Segmento Acessado: ${segmentName}`);
+
+    const frameNumber = strategy.translate(virtualAddress, pageTable, physicalMemory, config);
 
     const offset = virtualAddress & (pageSize - 1);
     const physicalAddress = (frameNumber << p) | offset;
 
-    console.log(` -> Endereço físico ${physicalAddress}`)
+    console.log(` -> Endereço físico ${physicalAddress}`);
 
     const outputString = `
 Endereço virtual: ${virtualAddress}
+Segmento Acessado: ${segmentName}
 Endereço físico: ${physicalAddress}
 Tabela de páginas: ${JSON.stringify(pageTable, null, 2)}
 Memória física: ${JSON.stringify(physicalMemory)}
 `;
-    fs.appendFileSync('saida.txt', outputString, 'utf8')
-
+    fs.appendFileSync("saida.txt", outputString, "utf8");
   } catch (error) {
-    console.error(`Erro ao processar endereço ${virtualAddress}: ${error.message}`)
+    console.error(`Erro ao processar endereço ${virtualAddress}: ${error.message}`);
     process.exit(1);
   }
-})
+});
 
-console.log("\n\n\n Resultaados detalhados salvos em 'saida.txt'")
-
-
-
-
-
-
+console.log("\n\n\n Resultaados detalhados salvos em 'saida.txt'");
--- pageTableHandler.js
+++ pageTableHandler.js
@@ -19,58 +19,105 @@ const oneLevelStrategy = {
 
     if (pageTable[pageNumber] !== -1) {
       frameNumber = pageTable[pageNumber];
-      console.log(` -> Acesso a pagina ${pageNumber}: MAPEADA para moldura ${frameNumber}`)
+      console.log(` -> Acesso a pagina ${pageNumber}: MAPEADA para moldura ${frameNumber}`);
     } else {
       console.log(` -> Acesso a pagina ${pageNumber}: NÃO MAPEADA. Procurando por uma moldura livre...`);
-      const freeFrameIndex = physicalMemory.findIndex(frame => frame === -1);
+      const freeFrameIndex = physicalMemory.findIndex((frame) => frame === -1);
       if (freeFrameIndex === -1) {
         throw new Error("Memória física lotada.");
       }
 
-      console.log(` -> Moldura livre encontrada: ${freeFrameIndex}. Alocando...`)
+      console.log(` -> Moldura livre encontrada: ${freeFrameIndex}. Alocando...`);
 
       pageTable[pageNumber] = freeFrameIndex;
       physicalMemory[freeFrameIndex] = pageNumber;
-      frameNumber = freeFrameIndex
+      frameNumber = freeFrameIndex;
     }
-    console.log(` -> Mapeamento final: Página ${pageNumber} -> Moldura ${frameNumber}`)
-    return frameNumber
-  }
-}
+    console.log(` -> Mapeamento final: Página ${pageNumber} -> Moldura ${frameNumber}`);
+    return frameNumber;
+  },
+};
 
 const twoLevelStrategy = {
   initialize: (numVirtualPages, config) => {
     console.log("Inicializando tabela de páginas de 2 níveis");
-    // TODO
-    return {}
+    const p1_size = Math.pow(2, config.p1_bits);
+
+    return {
+      directory: new Array(p1_size).fill(-1),
+      pageTables: [],
+    };
   },
+
   translate: (virtualAddress, pageTable, physicalMemory, config) => {
-    // TODO
-    return -1
-  }
-}
+    const p = config.pageSizeBits;
+    const p1_bits = config.p1_bits;
+    const total_vpn_bits = config.virtualAddressBits - p;
+    const p2_bits = total_vpn_bits - p1_bits;
+
+    const pageNumber = virtualAddress >> p;
+    const p1 = pageNumber >> p2_bits;
+    const p2_mask = Math.pow(2, p2_bits) - 1;
+    const p2 = pageNumber & p2_mask;
+
+    console.log(` -> Índices: p1=${p1}, p2=${p2}`);
+
+    let L2_table_index = pageTable.directory[p1];
+    let L2_table;
+
+    if (L2_table_index === -1) {
+      console.log(` -> Diretório [${p1}]: MISS. Criando Tabela de 2o nível...`);
+
+      const new_L2_table_size = Math.pow(2, p2_bits);
+      const new_L2_table = new Array(new_L2_table_size).fill(-1);
+
+      L2_table_index = pageTable.pageTables.push(new_L2_table) - 1;
+
+      pageTable.directory[p1] = L2_table_index;
+      console.log(` -> Tabela de 2o nível criada no índice ${L2_table_index}`);
+    }
+
+    L2_table = pageTable.pageTables[L2_table_index];
+
+    let frameNumber = L2_table[p2];
+
+    if (frameNumber !== -1) {
+      console.log(` -> Tabela L2 [${p2}]: HIT. Mapeada para moldura ${frameNumber}`);
+    } else {
+      console.log(` -> Tabela L2 [${p2}]: MISS. procurando por uma muldura livre...`);
+      const freeFrameIndex = physicalMemory.findIndex((frame) => frame === -1);
+      if (freeFrameIndex === -1) {
+        throw new Error("Memória física lotada.");
+      }
+
+      console.log(` -> Moldura livre encontrada: ${freeFrameIndex}. Alocando...`);
+
+      L2_table[p2] = freeFrameIndex;
+
+      physicalMemory[freeFrameIndex] = pageNumber;
+      frameNumber = freeFrameIndex;
+    }
+    console.log(` -> Mapeamento final: Página ${pageNumber} (p1=${p1}, p2=${p2}) -> Moldura ${frameNumber}`);
+    return frameNumber;
+  },
+};
 
 const invertedStrategy = {
   initialize: (numPhysicalFrames) => {
     console.log("Inicializando tabela de páginas invertida");
     // TODO
-    return []
+    return [];
   },
   translate: (virtualAddress, pageTable, physicalMemory, config) => {
     // TODO
-    return -1
-  }
+    return -1;
+  },
 };
 
-
 const pageTableStrategies = {
-  '1-level': oneLevelStrategy,
-  '2-level': twoLevelStrategy,
-  'inverted': invertedStrategy,
-}
+  "1-level": oneLevelStrategy,
+  "2-level": twoLevelStrategy,
+  inverted: invertedStrategy,
+};
 
 module.exports = { pageTableStrategies };
-
-
-
-
